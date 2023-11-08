fd4e158a8cd88c8f5dd63ecc8b8e6b7a7af0f969
nnasko <nnasskooo@gmail.com>
2023-11-08 17:21:59 +0000

    yhyh

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import Login from "@/components/Login";
+import React from "react";
+
+type Props = {
+  searchParams?: Record<"callbackUrl" | "error", string>;
+};
+
+const LoginPage = (props: Props) => {
+  return (
+    <Login
+      error={props.searchParams?.error}
+      callbackUrl={props.searchParams?.callbackUrl}
+    />
+  );
+};
+
+export default LoginPage;
